109bc2c570cce56764d87abeee39ad7fa026620e
YoAlex <alexzengxi@gmail.com>
2023-01-23 03:00:46 -0500

    update

--- Computational Project- Fast Fourier Transforms (FFT)/exericse 1.py
+++ Computational Project- Fast Fourier Transforms (FFT)/exericse 1.py
@@ -1,7 +1,9 @@
 # Computational Lab - Exercise 1
 # Alex Zeng, 1007099373, Jan 20th 2023
+from numpy import imag
+from numpy.fft import fftfreq
 
-save=False # if True then we save images as files
+save=True # if True then we save images as files
 
 from random import gauss
 import matplotlib.pyplot as plt
@@ -60,7 +62,52 @@ plt.show()
 n1 = len(x)
 n2 = len(y)
 n3 = len(z)
+
+delta = 1/N
 # Calculate frequencies of the transform in Hz
 freq1 = fftfreq(n1, delta)
+freq2 = fftfreq(n2, delta)
+freq3 = fftfreq(n3, delta)
 # Convert to angular frequencies, since the entire document is using it
-w = 2 * pi * freq
+w1 = 2 * np.pi * freq1
+w2 = 2 * np.pi * freq2
+w3 = 2 * np.pi * freq3
+
+fig, ( (ax7,ax8), (ax9, ax10) ) = plt.subplots(2,2,sharex='col',sharey='col')
+
+# ploting the graphs
+ax7.plot(w1, imag(z1))
+ax8.plot(w2, imag(z2))
+ax9.plot(w3, imag(z3))
+
+ax7.set_ylim(-600,600)
+ax7.set_xlim(-200,200)
+ax8.set_ylim(-100,100)
+ax8.set_xlim(-200,200)
+
+# remove the horizontal space between the top and bottom row
+fig.subplots_adjust(hspace=0)
+
+# labels
+ax7.set_ylabel('Imag(wave 1) after FFT')
+ax8.set_ylabel('Imag(wave 2) after FFT')
+ax9.set_ylabel('Imag(both waves) after FFT')
+ax7.set_xlabel('frequency of wave 1 [rad/s]')
+ax8.set_xlabel('frequency of wave 2 [rad/s]')
+ax9.set_xlabel('frequency of both waves [rad/s]')
+
+print("w1: max frequency is ", w1[np.argmax(imag(z1))],
+      "with the imaginary value being ", imag(z1).max())
+print("w2: max frequency is ", w2[np.argmax(imag(z2))],
+      "with the imaginary value being ", imag(z2).max())
+print("w3: max frequency is ", w3[np.argmax(imag(z3))],
+      "with the imaginary value being ", imag(z3).max())
+
+mydpi=800
+plt.tight_layout()
+
+if (save): plt.savefig('ex1_TwoWavesCombineWithFFT_MaxFreq.png',dpi=mydpi)
+plt.show()
+
+
+# END OF THE EXERCISE 1 SECTION
